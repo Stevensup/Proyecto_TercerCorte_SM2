eb21d7913ead4de6a97bb19265557d306359c3a1
Stevensup <buseche@unbosque.edu.co>
2023-05-11 18:41:39 -0500

    realizo un ajuste en clase start

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,32 +1,21 @@
 package co.edu.unbosque.controller;
 
-
 import co.edu.unbosque.view.VistaConsola;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
-
 public class Controller {
 
-    private Scanner scanner;
-    private VistaConsola vista;
+  private Scanner scanner;
+  private VistaConsola vista;
 
+  public Controller() {
+    scanner = new Scanner(System.in);
+    vista = new VistaConsola();
+  }
 
-	
-    public Controller() {
-        scanner = new Scanner(System.in);
-        vista = new VistaConsola();
-
-    }
-
-    
-    public void start() {
-      
-            }
-    
-       
+  public void start() {
+    //comentario
+  }
 }
-
-
